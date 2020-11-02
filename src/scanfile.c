0fa6c1bfb8088f9799a933fb2b619f9ff59621cc
Francesco Lazzarotto <fralaz1971@gmail.com>
2020-11-02 11:51:41 +0100

    added file input exercise

--- src/scanfile.c
+++ src/scanfile.c
@@ -4,10 +4,16 @@ int main(int argc, char *argv[]){
     if (argc == 2){
         fprintf(stderr, "reading from file %s\n", argv[1]);
         int a, b, ret;
-        printf("Please type two integer numbers separed by space:"); 
-        ret = scanf("%d %d", &a, &b); 
-        printf("n. of Input value read : %d\n", ret); 
-        printf("Input values read : a = %d, b = %d\n", a, b); 
+        
+        FILE* ptr = fopen(argv[1],"r"); 
+        if (ptr==NULL) 
+        { 
+            printf("no such file."); 
+            return EXIT_FAILURE; 
+        } 
+        char buf[100]; 
+        while (fscanf(ptr,"%*s %*s %s ",buf)==1) 
+            printf("%s\n", buf); 
         return 0;
     } else {
         fprintf(stderr, "usage: %s infile\n", argv[0]);
