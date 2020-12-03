e6523ba0f0d06419f97afc6352b6cfbf1ce5d293
Francesco Lazzarotto <fralaz1971@gmail.com>
2020-12-03 15:42:54 +0100

    added configure script for cross compiling on linux to mingw 32bit and 64bit MS windows targets

--- config/configurecrossi686mingw.sh
+++ config/configurecrossi686mingw.sh
@@ -3,7 +3,13 @@ echo "generating total makefile for $(uname) $OS ..." >/dev/stderr
 a=0;t=0;TARGETS="";RMTARGETS=""; EEXT=".exe";OEXT=".o"
 echo 'CC=i686-w64-mingw32-gcc -g '
 echo 'LD=i686-w64-mingw32-gcc '
-echo 'MAKE = mingw32-make'
+echo 'CXX  = i686-w64-mingw32-g++ -g '
+echo 'CXXFLAGS = '
+echo 'CFLAGS = '
+echo 'RM = rm -f'
+echo 'MV = mv '
+echo 'CP = cp '
+echo 'MAKE = make'
 echo 'SRC = $(wildcard src/*.c)'
 echo "OBJ = src/*.o"
 echo 'CPPFLAGS = -Isrc'
@@ -17,37 +23,34 @@ do
 	TARGET=$(basename ${t%.*})
 	echo 'TARGET'$a = $TARGET
 	TARGETS="$TARGET $TARGETS"
-	RMTARGETS="src/$TARGET $RMTARGETS"
+	RMTARGETS="$TARGET$EEXT $RMTARGETS"
 	a=$(($a+1))
 done
 echo 'TARGETS' = $TARGETS
 echo 'RMTARGETS' = $RMTARGETS
-echo 'all: $(TARGETS)'
+echo 'all: $(RMTARGETS)'
 a=0
 for s in $(ls -1 src/*.c)
 do
 	TARGET=$(basename ${s%.*})
-
-	echo 'obj/$(TARGET'$a')$(OEXT): src/$(TARGET'$a')'.c
-	echo -e "\t"'$(CC) -c  $< -o obj/'$TARGET'.o $(CPPFLAGS)'
-        if [[ $TARGET != "analysis" && $TARGET != "useanalysis"  ]]
+        if [[ $TARGET != "analysis" && $TARGET != "useanalysis" ]]
         then
-                echo '$(TARGET'$a'): obj/'$TARGET'$(OEXT)'
-                echo -e "\t"'$(CC) -o '$TARGET'$(EEXT) $< $(LDFLAGS)'
+                echo '$(TARGET'$a')$(EEXT): $(TARGET)'
+                echo -e "\t"'$(CC) $(CPPFLAGS) src/$(TARGET'$a').c  -o obj/'$TARGET$EEXT' $(LDFLAGS)'
         else
                 if [[ $TARGET == "useanalysis" ]]
                 then
-                        echo 'analysis$(EEXT):'
-	                echo -e "\t"
-                        echo 'obj/analysis$(OEXT):'
-                        echo -e "\t"'$(CC) $(CPPFLAGS) src/analysis.c  -c  -o src/analysis.o $(CPPFLAGS)'
-                        echo 'obj/$(TARGET'$a')$(OEXT):'
-                        echo -e "\t"'$(CC) $(CPPFLAGS) src/$(TARGET'$a').c  -c  '$TARGET'.o $(CPPFLAGS)'
+                        echo 'analysis$(EEXT): useanalysis.exe'
+                        echo -e "\t"'$(CP) useanalysis$(EEXT) analysis$(EEXT)'
+                        echo 'obj/analysis$(OEXT): src/analysis.c'
+                        echo -e "\t"'$(CC)  $(CPPFLAGS) -c src/analysis.c   -o obj/analysis.o '
+                        echo 'obj/$(TARGET'$a')$(OEXT): src/useanalysis.c'
+                        echo -e "\t"'$(CC)  $(CPPFLAGS) -c src/$(TARGET'$a').c  -o  obj/'$TARGET'.o '
                         echo '$(TARGET'$a')$(EEXT): obj/analysis$(OEXT) obj/$(TARGET'$a')$(OEXT)'
-                        echo -e "\t"'$(LD) obj/$(TARGET'$a')$(OEXT) obj/analysis$(OEXT)  -o '$TARGET'$(EEXT) $(LDFLAGS)'
+                        echo -e "\t"'$(LD) obj/$(TARGET'$a')$(OEXT) obj/analysis$(OEXT) -o '$TARGET'$(EEXT) $(LDFLAGS)'
                 fi
-
         fi
+
 	a=$(($a+1)) 
 done
 echo 'echo created all targets' >/dev/stderr
