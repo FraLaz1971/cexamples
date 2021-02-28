a246589398d7ec3ba1db5d5c725c1aa84cba8ce3
Francesco Lazzarotto <fralaz1971@gmail.com>
2021-02-28 14:34:35 +0100

    added intestation comment

--- src/sums.c
+++ src/sums.c
@@ -1,24 +1,30 @@
-//	this x program compute a sum of the first n integers
-//   taking n as input given on standard input
+/* sums.c 
+ * author: Francesco Lazzarotto 
+ * date: 2021-02-28
+ * email: fralaz1971@gmail.com 	
+ * version: 0.0.1
+ * this program compute a sum of the first n integers
+ * taking n from standard input
+ */
 #include <stdio.h>
 int main(){
 		int i, n, sum, ret;
 		sum = 0;
-// present program on beginning output screen
+/* present program on beginning output screen */
         puts("*****************************");
         puts("**         sums.f          **");
         puts("** summing first n numbers **");
         puts("*****************************");
-// prompt input request to user
+/* prompt input request to user */
         printf("Please, type an integer in [1:+INF)\n");
         scanf("%d", &n);
-        // do iteration loop on i from 1 to n
-        // of code included between 10 and 10 labels
+        /* do iteration loop on i from 1 to n
+         of code included between curly brackets */
 		for (i = 1;i<=n;i++){
 			sum = sum + i;
 			printf("i = %i sum = %d\n", i, sum);
         }
-        // print result on final output screen
+        /* print result on final output screen */
         puts("**************************************************");
         printf("** The sum of the first %d numbers is %i\n",i-1, sum);
         puts("**************************************************");
