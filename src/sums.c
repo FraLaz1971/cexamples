fbf4d9f87917ecec9f1deea70d09a5d94eb52d84
Francesco Lazzarotto <fralaz1971@gmail.com>
2021-02-28 14:51:25 +0100

    improved indentation

--- src/sums.c
+++ src/sums.c
@@ -1,15 +1,15 @@
 /* sums.c 
  * author: Francesco Lazzarotto 
  * date: 2021-02-28
- * email: fralaz1971@gmail.com 	
+ * email: fralaz1971@gmail.com     
  * version: 0.0.1
  * this program compute a sum of the first n integers
  * taking n from standard input
  */
 #include <stdio.h>
 int main(){
-		int i, n, sum, ret;
-		sum = 0;
+        int i, n, sum, ret;
+        sum = 0;
         /* present program on beginning output screen */
         puts("*****************************");
         puts("**         sums.c          **");
@@ -17,8 +17,6 @@ int main(){
         puts("*****************************");
         /* prompt input request to user */
         printf("Please, type an integer in [1:+INF)\n");
-        
-
     /* ret variable is scanf() Return Value and can be used for error handling. 
     * On success, the function returns the number of items of the argument list successfully read. 
     * If a reading error happens or the end-of-file is reached while reading, 
@@ -27,14 +25,14 @@ int main(){
         ret = scanf("%d", &n);
         /* do iteration loop on i from 1 to n
          of code included between curly brackets */
-		for (i = 1;i<=n;i++){
-			sum = sum + i;
-			printf("i = %i sum = %d\n", i, sum);
+        for (i = 1;i<=n;i++){
+            sum = sum + i;
+            printf("i = %i sum = %d\n", i, sum);
         }
         /* print result on final output screen */
         puts("**************************************************");
         printf("** The sum of the first %d numbers is %i\n",i-1, sum);
         puts("**************************************************");
         puts("End of the program. Bye.");
-		return 0;
+        return 0;
 }
